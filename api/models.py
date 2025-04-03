767e6aee0eea48b675c9630c49f42f6a66307ad0
SundarDong <brosisstudio123@gmail.com>
2025-04-03 20:35:53 +0545

    feat : Created New User Model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):  
+    age = models.IntegerField()
+    name = models.CharField(max_length=100)
+
+#Yesle lew chai string ma return garne ho Mathi ko Model Ko user ko details lai 
+def __str__(self):
+        return self.name
+
+
+
