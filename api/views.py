b5ee284c1221a1eca2b7901ac6819bdc2c7dd0dd
SundarDong <brosisstudio123@gmail.com>
2025-04-07 21:56:36 +0545

    feat : Create Update Endpoints

--- api/views.py
+++ api/views.py
@@ -29,3 +29,12 @@ def user_detail(request,pk):
         serializer= UserSerializer(user)
         return Response(serializer.data)
     
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    
+    
